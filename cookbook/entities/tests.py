4ee2614beb0dfae3473bca7547de92eb2d946b6c
iris9112 <iris9112@gmail.com>
2019-01-25 19:33:06 -0600

    first test

--- cookbook/entities/tests.py
+++ cookbook/entities/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
 
+from .models import Origin, Category
+
 # Create your tests here.
 
 # test models
+class OriginTestCase(TestCase):
+    def setUp(self):
+        self.origin_goku = Origin.objects.create(name='planeta sayayin',)
+    
+    def test_origin_creation(self):
+        self.assertTrue(isinstance(self.origin_goku, Origin))
+
+    def tearDown(self):
+        del self.origin_goku
 
 # test urls
 
