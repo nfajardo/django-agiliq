b981976feae1d8400c157bf3dceab93c66067565
iris9112 <iris9112@gmail.com>
2019-04-15 11:49:22 -0500

    create project basic

---
+++ django_multitenant/mytenant/tenants/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from tenant_schemas.models import TenantMixin
+
+
+class Tenant(TenantMixin):
+    """
+    Modelo que almacena los diferentes tenants
+    """
+    name = models.CharField(max_length=100)
+    # default true, schema will be automatically created and synced when it is saved
+    auto_create_schema = True
