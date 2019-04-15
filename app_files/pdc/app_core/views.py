f82a960af15e142bf9ddcc33ce7f3fd7c9f0ee2a
iris9112 <iris9112@gmail.com>
2019-04-14 21:03:43 -0500

    delete app files

--- app_files/pdc/app_core/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.generic import TemplateView
-
-# Create your views here.
-
-
-class HomeView(TemplateView):
-    template_name = "landing.html"
--- app_files/pdc/app_tenants/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from tenant_schemas.models import TenantMixin
-
-
-class Tenant(TenantMixin):
-    name = models.CharField(max_length=100)
